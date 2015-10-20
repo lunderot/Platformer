3b9eb4e74e6ac6419e3e7822eaccee560706ea80
lunderot <matriz@live.se>
2015-10-20 19:05:11 +0200

    Fixed Flipper rendering incorrectly

--- src/Flipper.cpp
+++ src/Flipper.cpp
@@ -31,7 +31,7 @@ void Flipper::Render(SDL_Renderer* renderer, const glm::mat4& cameraMatrix, floa
 		}
 
 		glm::vec4 translatedPoint0 = cameraMatrix * glm::vec4(renderPosition.x, renderPosition.y, 1, 1);
-		glm::vec4 translatedPoint1 = cameraMatrix * glm::vec4((renderPosition.x + length) * glm::cos(renderAngle), (renderPosition.y + length) * glm::sin(renderAngle), 1, 1);
+		glm::vec4 translatedPoint1 = cameraMatrix * glm::vec4(renderPosition.x + glm::cos(renderAngle) * length, renderPosition.y + glm::sin(renderAngle) * length, 1, 1);
 
 		SDL_SetRenderDrawColor(renderer, 0, 255, 0, SDL_ALPHA_OPAQUE);
 		SDL_RenderDrawLine(renderer,
